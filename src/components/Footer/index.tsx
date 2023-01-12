461848658f7d1f569104704000bf531ef824ca79
Deyvid William <deyvidwms@gmail.com>
2023-01-12 07:44:31 -0300

    botão de rolar para cima

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,44 @@
 import React, { useState, useEffect } from 'react';
 
-import { Container } from './styles';
+import { FaChevronUp } from 'react-icons/fa';
+
+import { Container, ButtonToTop } from './styles';
 
 const Footer: React.FC = () => {
   const [year, setYear] = useState<string>();
+  const [showButtonToTop, setShowButtonToTop] = useState<boolean>(false);
 
   useEffect(() => { 
     setYear( String( new Date().getFullYear() ) );
+
+    const scrollListener = () => {
+
+      const title = (document.getElementById('title')?.offsetTop || 0);
+      if (window.scrollY > title) {
+        setShowButtonToTop(true);
+      } else {
+        setShowButtonToTop(false);
+      }
+
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+
   }, []);
+
+  const toTop = () => {
+    window.scrollTo(0, 0);
+  };
 
   return (
     <Container>
+      <ButtonToTop isVisible={showButtonToTop} onClick={() => toTop()}>
+        <FaChevronUp />
+      </ButtonToTop>
       <p>MyPokeTeam {year}</p>
       <p>Desenvolvido por <span onClick={()=>{window.open('https://deyvid.dev.br')}}>Deyvid.dev</span></p>
     </Container>
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+type Props = {
+  isVisible: boolean;
+}
+
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -17,5 +21,29 @@ export const Container = styled.div`
     span {
       font-weight: bold;
     }
+  }
+`;
+
+export const ButtonToTop = styled.button`
+  position: fixed;
+  right: 50px;
+  bottom: 100px;
+  display: ${(props: Props) => (props.isVisible ? 'flex' : 'none')};
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  color: #FFF;
+  font-size: 1rem;
+  background-color: #1f6ddf;
+  border: 1px solid #1f6ddf;
+  border-radius: 3px;
+  box-shadow: 0 0 10px rgb(127 127 127 / 25%);
+  cursor: pointer;
+  transition: all ease 0.3s;
+  z-index: 1;
+
+  &:hover {
+    opacity: 0.9;
+    box-shadow: none;
   }
 `;
